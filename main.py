af7ea47988d1101910a22c07e9d6f896e398ac9c
János Rostás <rostas881122@gmail.com>
2025-07-13 13:21:44 +0100

    tested cha codes

--- main.py
+++ main.py
@@ -3,14 +3,20 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def print_char_code(line):
+    print([f"{c} -> {ord(c)}" for c in line])
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_char_code("# 1 Stock Market")
+    print_char_code("# 2 Stock Market")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
